d247970951baddad677d1de59400fa046fc03b84
DADAIZGOOD <dachord@hanyang.ac.kr>
2025-11-19 02:26:26 +0900

    prettierrc, eslint 수정

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -15,9 +16,43 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      import: importPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      //  import 자동 정렬 규칙
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          'newlines-between': 'always', // 그룹마다 한 줄 띄우기
+          alphabetize: {
+            order: 'asc',           // 알파벳 순 정렬
+            caseInsensitive: true,
+          },
+        },
+      ],
     },
   },
 ])
