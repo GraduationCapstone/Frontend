bbc85138208d3e65ce79182dc413c65b00b924c6
DADAIZGOOD <dachord@hanyang.ac.kr>
2025-11-28 19:21:27 +0900

    INIT: svg 설정

---
+++ src/types/svg.d.ts
@@ -0,0 +1,9 @@
+declare module "*.svg?react" {
+    import * as React from "react";
+    const Component: React.FC<React.SVGProps<SVGSVGElement>>;
+    export default Component;
+  }
+  declare module "*.svg" {
+    const src: string;
+    export default src;
+  }
