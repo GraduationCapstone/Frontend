7894a340eb1b483db896b379cd874eb0abe7747b
DADAIZGOOD <dachord@hanyang.ac.kr>
2025-12-23 19:30:21 +0900

    INIT: index 기존 컴포넌트 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,6 @@ export * from "./Button";
 export * from "./ListButton";
 export * from "./Chip";
 export * from "./CheckboxButton";
+export * from "./FloatingBtn";
+export * from "./ProfileIcon";
+export * from "./Tab";
