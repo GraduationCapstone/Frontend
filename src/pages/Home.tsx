4c5a35537b98af401e897497af2645ca8ce5379f
DADAIZGOOD <dachord@hanyang.ac.kr>
2025-12-27 20:12:10 +0900

    test: 테스트 코드 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,27 @@
-import { useState } from "react";
-import CheckboxButton from "../components/common/CheckButton/CheckButton"
+// src/pages/Home.tsx
+import FloatingBtn from '../components/common/FloatingBtn';
+import ProfileIcon from '../components/common/ProfileIcon';
 
 export default function Home() {
-  const [checked, setChecked] = useState(false);
-
   return (
-    <div className="p-8 flex items-center gap-4">
-      <CheckboxButton
-        ariaLabel="checkbox"
-        checked={checked}
-        onCheckedChange={setChecked}
-      />
+    <div className="flex h-screen flex-col items-center justify-center gap-4 bg-grayscale-gy100">
+      
+      {/* 1. 활성 상태 버튼 */}
+      <FloatingBtn onClick={() => alert('클릭!')}>
+        Btn
+      </FloatingBtn>
 
-      <CheckboxButton ariaLabel="checkbox disabled" disabled />
+      {/* 2. 비활성 상태 버튼 */}
+      <FloatingBtn disabled>
+        Btn
+      </FloatingBtn>
+
+      {/* 1. Active 상태 (초록색) */}
+        <ProfileIcon isActive={true} initial="U" />
+
+      {/* 2. Deactive 상태 (회색) */}
+      <ProfileIcon isActive={false} initial="U" />
+
     </div>
   );
 }
