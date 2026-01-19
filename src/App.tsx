9c0d67597a8581b371c40b9382d0dc3413717196
DADAIZGOOD <dachord@hanyang.ac.kr>
2026-01-20 01:05:00 +0900

    app.tsx 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/HomeController';
+import Landing from './pages/Landing';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 메인 페이지 */}
-        <Route path="/" element={<Home />} />
+        {/* 메인 페이지 (렌딩 페이지) */}
+        <Route path="/" element={<Landing />} />
+        {/*홈 페이지 */}
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
