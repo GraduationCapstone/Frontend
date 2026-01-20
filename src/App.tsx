ec4813e405e62c4fcc0e4eedfc07aefb728ba473
DADAIZGOOD <dachord@hanyang.ac.kr>
2026-01-20 20:58:48 +0900

    feat: landing, Login page 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/HomeController';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Intro from './pages/Intro';
 
 export default function App() {
   return (
@@ -8,8 +10,10 @@ export default function App() {
       <Routes>
         {/* 메인 페이지 (렌딩 페이지) */}
         <Route path="/" element={<Landing />} />
-        {/*홈 페이지 */}
+
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/intro" element={<Intro />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -4,7 +4,8 @@ import BackgroundImg from "../assets/bg/BG.mp4";
 
 export default function Landing() {
   return (
-    <div className="relative w-full bg-linear-to-b to-neutral-800/0 overflow-hidden overscroll-none">
+    <div className="relative w-full bg-linear-to-b to-white/0
+     overflow-hidden overscroll-none">
       <div className="absolute inset-0 -z-10">
         <video
           className="h-full w-full object-cover"
@@ -15,16 +16,16 @@ export default function Landing() {
           playsInline
         />
       </div>
-      <div className="pointer-events-none absolute inset-0 bg-linear-to-b from-grayscale-black" />
+      <div className="pointer-events-none absolute inset-0 bg-linear-to-b from-grayscale-white" />
       <div className="self-stretch w-full pt-16 relative flex min-h-screen flex-col items-center overflow-hidden justify-start">
-        <Header variant="transparent" />
+        <Header variant="default" />
         {/* Main */}
         <main className="self-stretch min-h-[63rem] flex-1 pt-72 inline-flex flex-col justify-start items-center gap-44">
           <section className="self-stretch flex flex-col justify-start items-center gap-8">
-            <h1 className="self-stretch text-center justify-center text-grayscale-white text-extra-ko">
+            <h1 className="self-stretch text-center justify-center text-grayscale-black text-extra-ko">
               Probe로 테스트를 자동화하세요.
             </h1>
-            <p className="self-stretch text-center justify-center text-grayscale-white text-h1-ko">
+            <p className="self-stretch text-center justify-center text-grayscale-black text-h1-ko">
               당신의 코드를 이해하는 AI, 테스트 워크플로를 하나로
             </p>
           </section>
