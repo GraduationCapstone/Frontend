cd15a3e257a59797b5959fcfd4c7b9cb1a9ad325
DADAIZGOOD <dachord@hanyang.ac.kr>
2026-03-27 16:49:36 +0900

    fix: 프로젝트 없을 때 글자 색 수정

--- src/pages/ProjectManagement/components/ProjectListSection.tsx
+++ src/pages/ProjectManagement/components/ProjectListSection.tsx
@@ -25,16 +25,15 @@ export default function ProjectListSection({
         <div className="w-xl px-5 pt-24 pb-14 rounded-2xl inline-flex flex-col justify-start items-center gap-14 shadow-is-100 bg-grayscale-white">
           <div className="inline-flex flex-col justify-start items-center gap-3">
             <div className="text-medium500-ko text-grayscale-black">참여 중인 프로젝트가 없습니다.</div>
-            <div className="text-medium500-ko text-grayscale-gy700 text-center">
+            <div className="text-medium500-ko text-grayscale-black text-center">
             Probe와 함께 테스트 자동화의 세상을 만들어봐요.</div>
-            {/* // 새프로젝트 누르면 어디로 가야 되지.... */}
-            <Button
+          </div>
+          <Button
             variant="staticGy900MText"
             label="새 프로젝트"
             children={undefined}
             onClick={onCreateProject}
           />
-          </div>
         </div>
       ) : (
         <div className="inline-flex flex-col justify-start items-start gap-8">
