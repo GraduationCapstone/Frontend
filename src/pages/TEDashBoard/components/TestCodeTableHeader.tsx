ab285523d69fc80b573a6129d9c85d4c33f94b87
DADAIZGOOD <dachord@hanyang.ac.kr>
2026-05-10 21:02:22 +0900

    fix: dashboard 테스트코드 테이블 헤더 레이아웃 수정

--- src/pages/TEDashBoard/components/TestCodeTableHeader.tsx
+++ src/pages/TEDashBoard/components/TestCodeTableHeader.tsx
@@ -8,6 +8,7 @@ type Props = {
 export default function TestCodeTableHeader({ onOpenSortMenu }: Props) {
   return (
     <div className="self-stretch w-full pl-4 pr-16 py-1 bg-grayscale-gy200 flex justify-start items-center gap-5">
+
       <SelectTrigger
         label="ID"
         widthClassName="w-layout-column-unit"
@@ -23,28 +24,24 @@ export default function TestCodeTableHeader({ onOpenSortMenu }: Props) {
       <SelectTrigger
         label="상태"
         widthClassName="w-size-min"
-        paddingClassName="px-3 py-2"
         onClick={(e) => onOpenSortMenu("status", e)}
       />
 
       <SelectTrigger
         label="테스트 시간"
         widthClassName="w-size-min"
-        paddingClassName="px-3 py-2"
         onClick={(e) => onOpenSortMenu("duration", e)}
       />
 
       <SelectTrigger
         label="테스터"
         widthClassName="w-size-min"
-        paddingClassName="px-3 py-2"
         onClick={(e) => onOpenSortMenu("user", e)}
       />
 
       <SelectTrigger
         label="일시"
         widthClassName="w-xs"
-        paddingClassName="px-3 py-2"
         onClick={(e) => onOpenSortMenu("date", e)}
       />
     </div>
@@ -76,18 +73,15 @@ export function SimpleTableHeader(props: Props) {
       <SelectTrigger
         label="상태"
         widthClassName="w-size-min"
-        paddingClassName="px-3 py-2"
         onClick={(e) => onOpenSortMenu(KEY_STATUS, e)}
       />
 
       <SelectTrigger
         label="테스트 시간"
         widthClassName="w-size-min"
-        paddingClassName="px-3 py-2"
         onClick={(e) => onOpenSortMenu(KEY_DURATION, e)}
       />
 
-      <div className="p-1 w-8" />
     </div>
   );
 }
