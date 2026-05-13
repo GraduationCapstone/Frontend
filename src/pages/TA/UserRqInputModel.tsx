3450fa35636eb1579f74401e0179538bbe8c2d65
DADAIZGOOD <dachord@hanyang.ac.kr>
2026-05-13 15:33:27 +0900

    fix: 선택된 브랜치 main -> develop

--- src/pages/TA/UserRqInputModel.tsx
+++ src/pages/TA/UserRqInputModel.tsx
@@ -146,7 +146,7 @@ export const useUserRqInputModel = () => {
           baseTestGroupName: testName,
           targetRepoId: Number(repoId),
           scenarioSerials: scenarioIdsAsStrings,
-          targetBranch: "main", // 하드코딩 반영
+          targetBranch: "develop", // 하드코딩 반영
           optionalServerUrl: state?.serverUrl, // 선택적으로 서버 URL 전달
         });
       });
